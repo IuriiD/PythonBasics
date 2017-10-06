f28e41ccce188de645454f24142972c9a6cbbc93
Iurii Dziuban <triptix@gmail.com>
2017-10-06 22:58:25 +0300

    practicepython-2

--- practicepython.org/1.Character Input.py
+++ practicepython.org/1.Character Input.py
@@ -1,5 +1,3 @@
-import datetime
-
 name = input('What is your name? ')
 age = int(input('What is your age? '))
 print(name,', you will turn 100 years old in', (100-age)+2017)
---
+++ practicepython.org/2.Odd Or Even.py
@@ -0,0 +1,5 @@
+number = int(input('Please, enter a number: '))
+if number % 2 == 0:
+	print('Number', number, 'is even.')
+else:
+	print('Number', number, 'is odd.')
